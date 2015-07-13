3fc896735fc252423e2af89770bf96cd0e1e956c
ethan <ethanmannette@gmail.com>
2015-07-13 13:19:05 -0600

    Finished javascript loops.

---
+++ loops.js
@@ -0,0 +1,29 @@
+console.log("1.");
+for (a = 0; a <= 1000; a += 100)
+console.log(a);
+
+console.log("2.");
+for (b = 1; b <= 128; b *= 2)
+console.log(b);
+
+console.log("3.");
+for (c = 0; c <= 10; c += 2)
+console.log(c);
+
+console.log("4.");
+for (d = 3; d <= 15; d += 3)
+console.log(d);
+
+console.log("5.");
+for (e = 9; e >= 0; e--)
+console.log(e);
+
+console.log("6.");
+for (f = 1; f < 5; f++)
+  for (g = 0; g < 3; g++)
+console.log(f);
+
+console.log("7.");
+for (h = 0; h < 3; h++)
+  for (i = 0; i < 5; i++)
+console.log(i);
